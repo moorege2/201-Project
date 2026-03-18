1347701e52627a3c3b57fde7f302581529111764
Abigail Burnham <burnhaar@miamioh.edu>
2026-03-18 19:28:53 -0400

    Force MongoDB Atlas URI in code

--- music-player/src/main/java/com/musicplayer/music_player/MusicPlayerApplication.java
+++ music-player/src/main/java/com/musicplayer/music_player/MusicPlayerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class MusicPlayerApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.data.mongodb.uri", 
+            "mongodb+srv://moorege2:25foot23@cluster0.xuq2e43.mongodb.net/musicdb?retryWrites=true&w=majority");
         SpringApplication.run(MusicPlayerApplication.class, args);
     }
 }
