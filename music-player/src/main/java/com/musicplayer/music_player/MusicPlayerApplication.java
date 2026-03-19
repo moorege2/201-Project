7d6313a9823747d590b2d4bc621a8c8b1c6fcab5
Abigail Burnham <burnhaar@miamioh.edu>
2026-03-19 10:04:58 -0400

    Fix MongoDB URI

--- music-player/src/main/java/com/musicplayer/music_player/MusicPlayerApplication.java
+++ music-player/src/main/java/com/musicplayer/music_player/MusicPlayerApplication.java
@@ -9,8 +9,13 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class MusicPlayerApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.data.mongodb.uri", 
-            "mongodb+srv://moorege2:25foot23@cluster0.xuq2e43.mongodb.net/musicdb?retryWrites=true&w=majority");
+        String mongoUri = System.getenv("MONGO_URI");
+        if (mongoUri != null) {
+            System.setProperty("spring.data.mongodb.uri", mongoUri);
+        } else {
+            System.setProperty("spring.data.mongodb.uri", 
+                "mongodb+srv://moorege2:25foot23@cluster0.xuq2e43.mongodb.net/musicdb?retryWrites=true&w=majority");
+        }
         SpringApplication.run(MusicPlayerApplication.class, args);
     }
 }
